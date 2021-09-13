77829d3a053ca96d5e58e962f7026c0cf9c00f83
illiakovtunevari <illia@evari.tech>
2021-09-13 17:11:44 +0300

    load

--- src/pages/AddPostPage.js
+++ src/pages/AddPostPage.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import { set } from 'lodash';
+import React, { useState } from 'react';
+import { usePostsSelector } from "customHooks/stateSelectors";
+
+
+
 
 const AddPostPage = () => {
+    const [value, setValue] = useState('');
+    const [content, setContent] = useState('');
+    const { list } = usePostsSelector();
+
 
     return (
         <form className='myForm'>
             <h2>Create post</h2>
-            <input type='text' placeholder='Title *' className='addPostInput'/>
-            <textarea cols='50' placeholder='Content *' className='addPostTA'></textarea>
+            <input type='text' placeholder='Title *' className='addPostInput' onChange={e => setValue(e.target.value)} />
+            <textarea cols='50' placeholder='Content *' className='addPostTA' onChange={e => setContent(e.target.value)} ></textarea>
             <button className='button submitBtn'>Add Post</button>
         </form>
     );

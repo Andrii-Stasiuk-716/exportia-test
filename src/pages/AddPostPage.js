7625821a2e3a26d1e87c72a8052467f822b7fa55
illiakovtunevari <illia@evari.tech>
2021-09-13 16:37:32 +0300

    upload test task

---
+++ src/pages/AddPostPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AddPostPage = () => {
+    
+    return (
+        <form className='myForm'>
+            <h2>Create post</h2>
+            <input type='text' placeholder='Title *' className='addPostInput'/>
+            <textarea cols='50' placeholder='Content *' className='addPostTA'></textarea>
+            <button className='button submitBtn'>Add Post</button>
+        </form>
+    );
+};
+
+export default AddPostPage;
